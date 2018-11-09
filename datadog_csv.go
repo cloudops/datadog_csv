673faf2abe1abde8e9d67fed21eb569607699777
Will Stevens <williamstevens@gmail.com>
2018-11-08 22:18:43 -0500

    added the CSV writer and added support for multdimenstional records

--- datadog_csv.go
+++ datadog_csv.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"log"
@@ -21,6 +22,7 @@ var (
 	outputFormat = "2006/01/02-15:04:05"
 	interval     time.Duration
 	intMap       = map[string]time.Duration{}
+	csvOut       *csv.Writer
 )
 
 // execution starts here...
@@ -31,7 +33,7 @@ func main() {
 	rangeStart := flag.String("start", "", "The starting point for the date range to query (format: yyyy/mm/dd-hh:mm) (required)")
 	rangeEnd := flag.String("end", "", "The ending point for the date range to query (format: yyyy/mm/dd-hh:mm) (required)")
 	intStr := flag.String("interval", "1h", "The perferred data interval. [5m, 10m, 20m, 30m, 1h, 2h, 4h, 8h, 12h, 24h]")
-	//csvFilepath := flag.String("csv_file", "", "The filepath of the CSV file to output to")
+	csvFilepath := flag.String("csv_file", "", "The filepath of the CSV file to output to")
 	version := flag.Bool("v", false, "Version of the binary (optional)")
 	iniflags.Parse()
 
@@ -65,6 +67,7 @@ func main() {
 	}
 
 	// initialize the interval mapping to a query duration
+	// in order to get a consistent interval, the period over which to query has to change
 	intMap["5m"], _ = time.ParseDuration("24h")    // 1d
 	intMap["10m"], _ = time.ParseDuration("48h")   // 2d
 	intMap["20m"], _ = time.ParseDuration("96h")   // 4d
@@ -80,7 +83,7 @@ func main() {
 	if intDur, inIntMap := intMap[*intStr]; inIntMap {
 		interval = intDur
 	} else {
-		log.Fatalf("The '-interval' value '%s' is not one of: 5m, 10m, 20m, 30m, 1h, 2h, 4h, 8h, 12h, 24h", *intStr)
+		log.Fatalf("The '-interval' value '%s' is not valid. Valid options are: 5m, 10m, 20m, 30m, 1h, 2h, 4h, 8h, 12h, 24h", *intStr)
 	}
 
 	// figure out the range that will provide an even multiple of the interval
@@ -122,21 +125,54 @@ func main() {
 	log.Printf("Requested date range: %s to %s", *rangeStart, *rangeEnd)
 	log.Printf("Querying date range: %s to %s", startTime.Format(inputFormat), endTime.Format(inputFormat))
 
+	// setup the output
+	if *csvFilepath == "" {
+		csvOut = csv.NewWriter(os.Stdout)
+	} else {
+		_ = os.Remove(*csvFilepath) // start by deleting the log file...
+		csvFile, err := os.OpenFile(*csvFilepath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		csvOut = csv.NewWriter(csvFile)
+	}
+
 	// get to querying...
-	tempTime := startTime
-	for tempTime.Before(endTime) {
-		log.Printf("Querying '%s' from '%s' to '%s'...\n", *query, tempTime.String(), tempTime.Add(interval))
-		details, err := client.QueryMetrics(tempTime.Unix(), tempTime.Add(interval).Unix(), *query)
+	tmpTime := startTime
+	initialize := true // only write the headers on the first set of queries
+	for tmpTime.Before(endTime) {
+		log.Printf("Querying '%s' from '%s' to '%s'...\n", *query, tmpTime.String(), tmpTime.Add(interval))
+		details, err := client.QueryMetrics(tmpTime.Unix(), tmpTime.Add(interval).Unix(), *query)
 		if err != nil {
 			log.Println("Failed to query DataDog metrics")
 			log.Fatal(err)
 		}
 
-		for _, data := range details {
-			for _, point := range data.Points {
-				fmt.Printf("%s, %f\n", time.Unix(int64(*point[0])/1000, 0).Format(outputFormat), *point[1])
+		if initialize {
+			headers := []string{"date"}
+			for _, data := range details {
+				headers = append(headers, *data.Scope)
+			}
+			csvOut.Write(headers)
+		}
+
+		// loop through query results
+		tmpOut := make([][]string, len(details[0].Points))
+		for d, data := range details {
+
+			for i, point := range data.Points {
+				if d == 0 { // initialize the csv row
+					tmpOut[i] = []string{
+						time.Unix(int64(*point[0])/1000, 0).Format(outputFormat),
+						fmt.Sprintf("%f", *point[1]),
+					}
+				} else { // two dimensional data, so append to the csv row
+					tmpOut[i] = append(tmpOut[i], fmt.Sprintf("%f", *point[1]))
+				}
 			}
 		}
-		tempTime = tempTime.Add(interval)
+		csvOut.WriteAll(tmpOut)
+		tmpTime = tmpTime.Add(interval)
+		initialize = false
 	}
 }
